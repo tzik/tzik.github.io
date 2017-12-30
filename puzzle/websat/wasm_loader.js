ce72a1c575ea0c7efe756fb0984de5399a3852e6
tzik <mail@tzik.jp>
2017-12-31 08:26:28 +0900

    fix

--- puzzle/websat/wasm_loader.js
+++ puzzle/websat/wasm_loader.js
@@ -1,8 +1,8 @@
 
 export async function loadWasm(url) {
   let relative_to = 'https://tzik.github.io/puzzle/websat/wasm_loader.js';
-  // let response = fetch(new URL(url, import.meta.url));
-  let response = fetch(new URL(url, import.meta.url));
+  // let relative_to = import.meta.url;
+  let response = fetch(new URL(url, relative_to));
   try {
     return WebAssembly.compileStreaming(response);
   } catch (e) {
